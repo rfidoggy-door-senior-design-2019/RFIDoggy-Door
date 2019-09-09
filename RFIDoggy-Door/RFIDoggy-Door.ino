1183e15f36897f39097a78a6e2f061434bd5524b
Brennan <bmkk85@mst.edu>
2019-09-09 18:26:44 -0500

    Interfacing with two rfid transmitters

--- RFIDoggy-Door/RFIDoggy-Door.ino
+++ RFIDoggy-Door/RFIDoggy-Door.ino
@@ -18,14 +18,12 @@
 // Soil Moisture Sensor
 #define AI_MOIST_SENSOR_PIN   A1
 
-// Solenoid
-#define DO_SOLENOID_PIN       8
-
 // Speaker
 #define PWM_SPEAKER_PIN       3
 
 // MFRC522 RFID Reader
-#define PWM_SS_PIN            10
+#define PWM_SDA_PIN1          10
+#define PWM_SDA_PIN2          8
 #define PWM_RST_PIN           9
 
 // HC-05 Bluetooth Module
@@ -37,15 +35,17 @@
 #define AI_SCL_PIN            A5
 
 /*****************************************************************************/
+#define NR_OF_READERS         2
 
-MFRC522 mfrc522(PWM_SS_PIN, PWM_RST_PIN); // Create MFRC522 instance.
+byte SDAPins[] = {PWM_SDA_PIN1, PWM_SDA_PIN2};
+
+MFRC522 mfrc522[NR_OF_READERS]; // Create MFRC522 instance.
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-
   // outputs
-  pinMode(DO_SOLENOID_PIN, OUTPUT);
+  //pinMode(DO_SOLENOID_PIN, OUTPUT);
   //pinMode(PWM_SPEAKER_PIN, OUTPUT);
 
   Serial.begin(9600);
@@ -55,11 +55,59 @@ void setup()
 
   // initialize SPI bus and MFRC522
   SPI.begin();
-  mfrc522.PCD_Init();
+
+
+  for(uint8_t reader = 0; reader<NR_OF_READERS; reader++)
+  {
+     mfrc522[reader].PCD_Init(SDAPins[reader], PWM_RST_PIN);
+    Serial.print(F("Reader "));
+    Serial.print(reader);
+    Serial.print(F(": "));
+    mfrc522[reader].PCD_DumpVersionToSerial();
+  }
+
+  
 }
 
 // the loop function runs infinitely while the board is powered
 void loop()
-{
+{ 
+    String tag;
    // TODO: implement RFIDoggy Door control
+    for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+
+      // Looking for new cards
+      if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
+        Serial.print(F("Reader "));
+        Serial.print(reader);
+
+        // Show some details of the PICC (that is: the tag/card)
+        Serial.print(F(": Card UID:"));
+        tag = printTagUID(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
+        Serial.println(tag);
+        if(tag == "8be5c8c")
+        {
+          //Serial.println("SUCCESS");
+          
+        }
+        else
+        {
+          //Serial.println("FAIL");
+        }
+      }
+      //delay(50);
+    }
+   
+ 
+}
+
+
+String printTagUID(byte * buffer, byte bufferSize) {
+  String tag;
+  for (byte i = 0; i < bufferSize; i++) {
+    //Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    //Serial.print(buffer[i], HEX);
+     tag.concat(String(buffer[i], HEX));
+  }
+  return tag;
 }
