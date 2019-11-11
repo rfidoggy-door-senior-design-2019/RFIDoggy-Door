99b27a318560e6d5ea99afafcb31f63ddb3dae53
Jared Hanisch <jwhbd2@mst.edu>
2019-11-11 10:06:22 -0600

    nvm added a quotation mark by mistake, removing it.

--- RFIDoggy-Door/RFIDoggy-Door.ino
+++ RFIDoggy-Door/RFIDoggy-Door.ino
@@ -99,7 +99,6 @@ void loop()
           
           if(tag == "8be5c8c" && incomingCharacter == 'U'){
             Serial.println("SUCCESS");
-            "
             digitalWrite(OUTSIDE_SOL, HIGH);
             delay(2000);
             digitalWrite(OUTSIDE_SOL, LOW);
